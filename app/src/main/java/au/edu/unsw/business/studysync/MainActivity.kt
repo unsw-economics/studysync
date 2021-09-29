08c824fc757458343bdd6ce6e5f1cf96c33c3ade
alkhe <raphaeljmu@gmail.com>
2021-09-30 02:47:39 +1000

    add UI upgrades

--- app/src/main/java/au/edu/unsw/business/studysync/MainActivity.kt
+++ app/src/main/java/au/edu/unsw/business/studysync/MainActivity.kt
@@ -25,6 +25,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        supportActionBar?.hide()
+
         vm = ViewModelProvider(
             this,
             MainViewModelFactory(preferences, database)
