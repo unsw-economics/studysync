4f138d6a303dbbaea3458cfc1c738c2f381a956f
Zhuoling Zhou <16379978+ZhouZ-1@users.noreply.github.com>
2022-05-04 12:06:54 +1000

    Remove suspend keyword to fix error
    Was able to fix the below error when I removed this keyword.
    error: Not sure how to convert a Cursor to this method's return type (java.lang.Object)

--- app/src/main/java/au/edu/unsw/business/studysync/database/ReportDao.kt
+++ app/src/main/java/au/edu/unsw/business/studysync/database/ReportDao.kt
@@ -8,17 +8,17 @@ import androidx.room.Query
 @Dao
 interface ReportDao {
     @Query("select AR.* from reports R join app_reports AR on (R.period = AR.period and R.day = AR.day)")
-    suspend fun getAllAppReports(): List<DbAppReport>
+    fun getAllAppReports(): List<DbAppReport>
 
     @Query("select AR.* from reports R join app_reports AR on (R.period = AR.period and R.day = AR.day) where R.period = 'experiment'")
-    suspend fun getExperimentAppReports(): List<DbAppReport>
+    fun getExperimentAppReports(): List<DbAppReport>
 
     @Query("select AR.* from reports R join app_reports AR on (R.period = AR.period and R.day = AR.day) where R.synced = 0")
-    suspend fun getUnsyncedAppReports(): List<DbAppReport>
+    fun getUnsyncedAppReports(): List<DbAppReport>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertMultipleDayReports(reports: List<DbReport>, appReports: List<DbAppReport>)
+    fun insertMultipleDayReports(reports: List<DbReport>, appReports: List<DbAppReport>)
 
     @Query("update reports set synced = 1 where period = :period and day = :day")
-    suspend fun markReportSynced(period: String, day: Int)
+    fun markReportSynced(period: String, day: Int)
 }
